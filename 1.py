06d1476e40336814836e116608241d6f8711f608
JPhat <trandoducphat@gmail.com>
2026-06-24 14:26:17 +0700

    Initial for main branch

---
+++ 1.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+lst = [1,2,3,4,5]
+
+print(lst)
---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
